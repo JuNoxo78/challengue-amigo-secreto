4abcbd2f4b12841e20c5bde884c6e1fa4018652b
Juan López <juanandrelopezcordova8@gmail.com>
2025-02-21 00:57:05 -0500

    v1.0

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# Challengue "Amigo Secreto" de Oracle ONE 2025 G8
+# Amigo Secreto
 
-Con esto proyecto, se busca practicar la lógica de programación básica.
+## Descripción del Proyecto
+Este proyecto es una aplicación web sencilla para organizar un sorteo de "Amigo Secreto". Permite a los usuarios ingresar los nombres de sus amigos y realizar un sorteo aleatorio para asignar un amigo secreto a cada participante.
+
+## Funcionalidades
+- Agregar nombres de amigos a una lista.
+- Visualizar los nombres ingresados.
+- Realizar un sorteo aleatorio y mostrar el amigo secreto seleccionado.
+
+## Cómo Usarlo
+1. Ingrese el nombre de un amigo en el campo de texto.
+2. Haga clic en el botón "Añadir" para agregarlo a la lista.
+3. Repita el proceso para todos los participantes.
+4. Haga clic en el botón "Sortear amigo" para realizar el sorteo.
+5. El nombre del amigo secreto sorteado se mostrará en pantalla.
+
+## Soporte
+Si encuentra problemas o tiene preguntas sobre el proyecto, puede consultar la documentación o abrir un issue en este repositorio.
+
+## Autor
+Este proyecto fue desarrollado por Juan López.
--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputFriends = document.getElementById("amigo");
+const listFriends = document.getElementById("listaAmigos");
+const sortedFriend = document.getElementById("resultado")
+
+let amigos = [];
+
+function agregarAmigo() {
+    if (inputFriends.value != "") {
+        amigos.push(inputFriends.value);
+        listarAmigos();
+    } else {
+        alert("El nombre no es válido");
+    }
+    inputFriends.value = "";
+}
+
+function listarAmigos() {
+    listFriends.innerHTML = "";
+
+    for (let index = 0; index < amigos.length; index++) {
+        let element = document.createElement("li");
+        element.textContent = amigos[index];
+        listFriends.appendChild(element);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length != 0) {
+        let randomIndex = Math.floor(Math.random() * amigos.length);
+        listFriends.innerHTML = "";
+        resultado.innerHTML = `El amigo sorteado es ${amigos[randomIndex]}.`
+    } else {
+        alert("Tiene que ingresar algún amigo.")
+    }
+}
